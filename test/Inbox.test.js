f9e67135f0fd682a91e77bcf1eff52492f9a9a96
davidgk <dkotlirevsky@newclassrooms.org>
2022-02-04 09:21:14 -0300

    First test running obtaining accounts

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,22 @@
 // contract test code will go here
+const assert = require('assert');
+const {expect} = require('chai');
+// Ethereum test network
+const ganache = require('ganache-cli');
+// how to communicate our code with that.
+const Web3 = require('web3')
+
+const localNetworkProvider = ganache.provider();
+const web3 = new Web3(localNetworkProvider)
+
+describe ('Inbox Contract tests', () => {
+    let accountToDeploy, accounts
+    beforeEach(async () => {
+        // Get a list of all accounts
+        accounts = await web3.eth.getAccounts();
+    })
+    it( 'obtain accounts from web3', () => {
+       expect(accounts.length > 0).to.be.true;
+    })
+    it( 'with an account we deploy a contract', () => {})
+})

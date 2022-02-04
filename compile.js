10a155b3552a0cce00854553f44809f4aa70154b
davidgk <dkotlirevsky@newclassrooms.org>
2022-02-04 11:05:11 -0300

    First tests from deploy contract
    * modify compile file to be executed as method and on demand
    * Modify contract to use variable public as access method ( solidity
      feature)
    * add chai as test dependency
    * add tests for getting method and some important comments about
      implementation

--- compile.js
+++ compile.js
@@ -7,5 +7,5 @@ const solCompiler = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-module.exports = solCompiler.compile(source, 1).contracts[':Inbox'];
+module.exports = () => solCompiler.compile(source, 1).contracts[':Inbox'];
 
